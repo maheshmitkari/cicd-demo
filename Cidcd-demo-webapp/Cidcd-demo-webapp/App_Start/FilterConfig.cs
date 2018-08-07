43f6e2d2ff43aa8d71a373b1250816f3d9d207b6
Mahesh Mitkari <m_mahesh@msn.com>
2018-08-07 20:08:19 +0530

    First Commit/Push to master added asp.net web project, consoleApp1 project

---
+++ Cidcd-demo-webapp/Cidcd-demo-webapp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Cidcd_demo_webapp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
